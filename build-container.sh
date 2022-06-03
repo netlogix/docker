18f8c1bc391a492a02fb49bf7eb52e9ae709a914
Sascha Nowak <sascha.nowak@netlogix.de>
2022-06-03 16:02:23 +0800

    FEATURE: Switch to ubuntu for php docker images

--- build-container.sh
+++ build-container.sh
@@ -15,14 +15,14 @@ DOCKER_BUILDKIT=1 docker build -t elasticsearch:${VERSION} -f containers/elastic
 DOCKER_BUILDKIT=1 docker build -t webserver-neos:${VERSION} -f containers/webserver/Dockerfile --target=webserver-neos containers/webserver
 DOCKER_BUILDKIT=1 docker build -t webserver-static:${VERSION} -f containers/webserver/Dockerfile --target=webserver-static containers/webserver
 
-DOCKER_BUILDKIT=1 docker build -t php-7.4-fpm:${VERSION} --build-arg PHP_VERSION=7.4 -f containers/php/Dockerfile --target=php-fpm containers/php
-DOCKER_BUILDKIT=1 docker build -t php-7.4-cli:${VERSION} --build-arg PHP_VERSION=7.4 -f containers/php/Dockerfile --target=php-cli containers/php
-DOCKER_BUILDKIT=1 docker build -t php-7.4-cron:${VERSION} --build-arg PHP_VERSION=7.4 -f containers/php/Dockerfile --target=php-cron containers/php
+DOCKER_BUILDKIT=1 docker build -t php-7.4-fpm:${VERSION} --build-arg PHP_VERSION=7.4 --build-arg UBUNTU_VERSION=20.04 -f containers/php/Dockerfile --target=php-fpm containers/php
+DOCKER_BUILDKIT=1 docker build -t php-7.4-cli:${VERSION} --build-arg PHP_VERSION=7.4 --build-arg UBUNTU_VERSION=20.04 -f containers/php/Dockerfile --target=php-cli containers/php
+DOCKER_BUILDKIT=1 docker build -t php-7.4-fpm-dev:${VERSION} --build-arg PHP_VERSION=7.4 --build-arg UBUNTU_VERSION=20.04 -f containers/php/Dockerfile --target=php-fpm-dev containers/php
+DOCKER_BUILDKIT=1 docker build -t php-7.4-cli-dev:${VERSION} --build-arg PHP_VERSION=7.4 --build-arg UBUNTU_VERSION=20.04 -f containers/php/Dockerfile --target=php-cli-dev containers/php
+DOCKER_BUILDKIT=1 docker build -t php-7.4-cron:${VERSION} --build-arg PHP_VERSION=7.4 --build-arg UBUNTU_VERSION=20.04 -f containers/php/Dockerfile --target=php-cron containers/php
 
-DOCKER_BUILDKIT=1 docker build -t php-8.0-fpm:${VERSION} --build-arg PHP_VERSION=8.0 -f containers/php/Dockerfile --target=php-fpm containers/php
-DOCKER_BUILDKIT=1 docker build -t php-8.0-cli:${VERSION} --build-arg PHP_VERSION=8.0 -f containers/php/Dockerfile --target=php-cli containers/php
-DOCKER_BUILDKIT=1 docker build -t php-8.0-cron:${VERSION} --build-arg PHP_VERSION=8.0 -f containers/php/Dockerfile --target=php-cron containers/php
-
-DOCKER_BUILDKIT=1 docker build -t php-8.1-fpm:${VERSION} --build-arg PHP_VERSION=8.1 -f containers/php/Dockerfile --target=php-fpm containers/php
-DOCKER_BUILDKIT=1 docker build -t php-8.1-cli:${VERSION} --build-arg PHP_VERSION=8.1 -f containers/php/Dockerfile --target=php-cli containers/php
-DOCKER_BUILDKIT=1 docker build -t php-8.1-cron:${VERSION} --build-arg PHP_VERSION=8.1 -f containers/php/Dockerfile --target=php-cron containers/php
+DOCKER_BUILDKIT=1 docker build -t php-8.1-fpm:${VERSION} --build-arg PHP_VERSION=8.1 --build-arg UBUNTU_VERSION=22.04 -f containers/php/Dockerfile --target=php-fpm containers/php
+DOCKER_BUILDKIT=1 docker build -t php-8.1-cli:${VERSION} --build-arg PHP_VERSION=8.1 --build-arg UBUNTU_VERSION=22.04 -f containers/php/Dockerfile --target=php-cli containers/php
+DOCKER_BUILDKIT=1 docker build -t php-8.1-fpm-dev:${VERSION} --build-arg PHP_VERSION=8.1 --build-arg UBUNTU_VERSION=22.04 -f containers/php/Dockerfile --target=php-fpm-dev containers/php
+DOCKER_BUILDKIT=1 docker build -t php-8.1-cli-dev:${VERSION} --build-arg PHP_VERSION=8.1 --build-arg UBUNTU_VERSION=22.04 -f containers/php/Dockerfile --target=php-cli-dev containers/php
+DOCKER_BUILDKIT=1 docker build -t php-8.1-cron:${VERSION} --build-arg PHP_VERSION=8.1 --build-arg UBUNTU_VERSION=22.04 -f containers/php/Dockerfile --target=php-cron containers/php
