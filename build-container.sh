855e8f3161757524e6583033b2e9a1bfef1210b8
Tim Weisenberger <tim.weisenberger@netlogix.de>
2022-10-27 16:27:52 +0200

    FEATURE: Add Chromium container

--- build-container.sh
+++ build-container.sh
@@ -19,6 +19,7 @@ docker buildx build -t styleguide:${VERSION} -f containers/styleguide/Dockerfile
 docker buildx build -t webserver-neos:${VERSION} -f containers/webserver/Dockerfile --target=webserver-neos containers/webserver
 docker buildx build -t webserver-static:${VERSION} -f containers/webserver/Dockerfile --target=webserver-static containers/webserver
 docker buildx build -t flow-debugproxy:${VERSION} -f containers/flow-debugproxy/Dockerfile containers/flow-debugproxy
+docker buildx build -t chromium:${VERSION} -f containers/chromium/Dockerfile containers/chromium
 
 docker buildx build -t php-7.2-fpm:${VERSION} --build-arg PHP_VERSION=7.2 --build-arg UBUNTU_VERSION=18.04 -f containers/php/Dockerfile --target=php-fpm containers/php
 docker buildx build -t php-7.2-cli:${VERSION} --build-arg PHP_VERSION=7.2 --build-arg UBUNTU_VERSION=18.04 -f containers/php/Dockerfile --target=php-cli containers/php
